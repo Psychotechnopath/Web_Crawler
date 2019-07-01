02cebbea2d83685c1063bb24fdd6566d8df4479e
Psychotechnopath <ymevdlinden@gmail.com>
2019-07-01 15:46:57 +0200

    Runescape Items displayen

--- web_crawler.py
+++ web_crawler.py
@@ -1,4 +1,28 @@
-print("Dit is de eerste test")
-print("dit is de tweede test")
-print("Dit is de derde test")
-print("Test nummero vier (NONDEJU)")
+# importing libraries
+from bs4 import BeautifulSoup
+import urllib.request
+import time
+
+url = "http://services.runescape.com/m=itemdb_oldschool/viewitem?obj="
+
+
+item_list = []
+
+for i in range(400, 450):
+    try:
+        page = urllib.request.urlopen(url + str(i))
+        print("Requesting page " + url + str(i))
+        soup = BeautifulSoup(page, 'html.parser')
+        item_list.append(soup.title)
+        time.sleep(1)
+    except urllib.error.HTTPError:
+        print("Bad request")
+
+for i in item_list:
+    if str(i) == "<title>RuneScape Oldschool - Grand Exchange - Prices, Trade, Market Movers</title>":
+        continue
+    else:
+        print(i + item_list.index(object))
+
+
+
